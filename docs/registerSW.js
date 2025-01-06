3b15d719c34df733cdce9d506b6a4b0fd7cfb30b
pobu168 <244202969@qq.com>
2025-01-06 11:45:01 +0800

    update: 0106.1

--- docs/registerSW.js
+++ docs/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/1024/sw.js', { scope: '/1024/' })})}
